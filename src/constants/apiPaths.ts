1029cde8cbe828fd7de8dfc5c2bce66781d07ff1
Denis Shiriaev <denis.shiriaev@novartis.com>
2020-12-07 22:58:05 +0300

    Update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://8hsosw75y7.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://kv28uhouy2.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://8hsosw75y7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://tambovchanin-cart-api-dev.eu-west-1.elasticbeanstalk.com/',
+  cart: 'https://tambovchanin-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
